7e921ec8a2a7ccc6c0bcf843708ed9c282ed0153
ctwohofakit <ctwohofa@gmail.com>
2026-06-06 09:35:49 -0700

    Initial Commit

---
+++ WeatherIOSApp/WeatherIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIOSAppApp.swift
+//  WeatherIOSApp
+//
+//  Created by Kit Sito on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
